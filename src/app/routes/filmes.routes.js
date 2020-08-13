d237cf4d864a3dc1303be3a9dabbf74ce8279f0c
stella-celentano <stellacelentano99@gmail.com>
2020-08-13 20:57:56 -0300

    Disponibilizando rota criar filme

---
+++ src/app/routes/filmes.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+// dentro da interface Router tem todos os métodos de rotas disponíveis
+const route = express.Router(); 
+
+const FilmeController = require('./../controllers/filmes.controller');
+
+route.post('criar', FilmeController.criarFilme)
+// síntaxe: route.método('nome_da_rota', controller_responsável.nome_do_método_no_controller) 
+// essa rota redireciona direto para o método
+
+module.exports = route
