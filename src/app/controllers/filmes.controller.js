d20fe04e26b78312f27d3b8765537ddfa8fc5059
stella-celentano <stellacelentano99@gmail.com>
2020-08-13 20:51:02 -0300

    Implementando o criar filme no controller de filmes

---
+++ src/app/controllers/filmes.controller.js
@@ -0,0 +1,20 @@
+const filmesschema = require('./../models/filmes.model');
+
+class Filme {
+    criarFilme(req, res) { 
+        // dizendo para a constando body que ela vai receber o valor da requisição na posição body,
+        // tudo que for enviado no corpo da requisição vai estar nessa constante
+        const body = req.body
+
+        filmesschema.create(body, (err, data) => {
+            if (err) {
+                res.status(500).send({ message: "Houve um erro ao processar sua requisição", error: err })
+            } else {
+                res.status(201).send({ message: "Filme criado com sucesso no banco de dados", filme: data })
+            }
+        })
+    }
+}
+
+// já exporta a classe instanciada, para quem importar ja tem acesso à todos os método
+module.exports = new Filme() 
