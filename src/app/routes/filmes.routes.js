29c30e036be88e813f405525f02211a8aeeb2fdb
stella-celentano <stellacelentano99@gmail.com>
2020-08-13 21:06:02 -0300

    Adicionando a / à rota criar

--- src/app/routes/filmes.routes.js
+++ src/app/routes/filmes.routes.js
@@ -4,8 +4,8 @@ const route = express.Router();
 
 const FilmeController = require('./../controllers/filmes.controller');
 
-route.post('criar', FilmeController.criarFilme)
+route.post('/criar', FilmeController.criarFilme)
 // síntaxe: route.método('nome_da_rota', controller_responsável.nome_do_método_no_controller) 
-// essa rota redireciona direto para o método
+// essa rota redireciona direto para o método e executa o endpoint
 
 module.exports = route
