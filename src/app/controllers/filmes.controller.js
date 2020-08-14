d0a8fb52ad8d617cff0eeeffdbc5423db7369aa6
stella-celentano <stellacelentano99@gmail.com>
2020-08-13 21:42:56 -0300

    Implementando visualizar um filme no controller de filmes

--- src/app/controllers/filmes.controller.js
+++ src/app/controllers/filmes.controller.js
@@ -24,6 +24,21 @@ class Filme {
             }
         })
     }
+
+    visualizarUmFilme(req, res) {
+        // esse parâmetro da a possibilidade do usuário informar o nome do filme que será buscado
+        // quando a requisição chegar, irá ler os parâmetros para realizar essa requisição
+        const nome = req.params.nome
+
+        // vai buscar dentro do banco de dados o name cadastraco que seja igual ao nome que está sendo buscado
+        filmesschema.find({ nome: nome }, (err, data) => {
+            if (err) {
+                res.status(500).send({ message: "Houve um erro ao processar sua requisição", error: err })
+            } else {
+                res.status(200).send({ message: `Filme ${nome} foi recuperado com sucesso`, filme: data })
+            }
+        })
+    }
 }
 
 // já exporta a classe instanciada, para quem importar ja tem acesso à todos os método
