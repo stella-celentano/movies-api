ccf49f5bcf19705a220607f2ad905053bfcdd521
stella-celentano <stellacelentano99@gmail.com>
2020-08-24 21:48:55 -0300

    Implementando controller para buscar filmes de um diretor

--- src/app/controllers/diretor.controller.js
+++ src/app/controllers/diretor.controller.js
@@ -15,7 +15,31 @@ class Diretor {
                         res.status(200).send({ message: "Diretores recuperados com sucesso", data: data })
                     }
                 }
-            }) 
+            })
+    }
+
+    buscarOsFilmesDeUmDiretorPeloNomeDele(req, res) {
+        const { nomeDiretor } = req.params
+
+        if (nomeDiretor == undefined || nomeDiretor == 'null') {
+            res.status(400).send({ message: "O nome do diretor deve ser obrigatoriamente preenchido" })
+        }
+
+        diretor.find({ nome: nomeDiretor })
+            .populate('filmes', { nome: 1, imagem: 1 }) //vai popular com o ID, nome e imagem do filme
+            .exec((err, data) => {
+                if (err) {
+                    res.status(500).send({ message: "Houve um erro ao processar a sua requisição", error: err })
+                } else {
+                    if (data.length <= 0) {
+                        res.status(200).send({message: `O diretor ${nomeDiretor} não existe no banco de dados`})
+                    } else if (data['filmes'].length <= 0) {
+                        res.status(200).send({ message: `O diretor ${nomeDiretor} não possui nenhum filme cadastrado` })
+                    } else {
+                        res.status(200).send({ message: `O diretor ${nomeDiretor} possui filmes cadastrados`, data: data })
+                    }
+                }
+            })
     }
 
 }
