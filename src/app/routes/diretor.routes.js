4adb27d794dd3d70b7db6619d0c12857b2b4dda5
stella-celentano <stellacelentano99@gmail.com>
2020-08-24 21:49:08 -0300

    Implementando a rota para buscar um diretor

--- src/app/routes/diretor.routes.js
+++ src/app/routes/diretor.routes.js
@@ -3,5 +3,6 @@ const route = express.Router();
 const Diretor = require('./../controllers/diretor.controller');
 
 route.get('/listarTodos', Diretor.buscarTodosOsDiretores);
+route.get('/listarUm/:nomeDiretor', Diretor.buscarOsFilmesDeUmDiretorPeloNomeDele);
 
 module.exports = route;
